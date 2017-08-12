4a55d6fcb25d47c00b8813d0bfe41783b0827575
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-08-12 17:53:46 +0300

    Set app content in hero

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,26 @@
 import * as React from 'react';
 import './App.css';
 
-import { Container, Box, Title } from 'bloomer';
+import { Container, Box, Title, Hero, HeroBody } from 'bloomer';
 
 const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
     return (
-      <Container className="App">
-        <Box className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Title>Welcome to React</Title>
-        </Box>
-        <Box className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </Box>
-      </Container>
+      <Hero isFullHeight>
+        <HeroBody hasTextAlign="centered">
+          <Container className="App">
+            <Box className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <Title>Welcome to React</Title>
+            </Box>
+            <Box className="App-intro">
+              To get started, edit <code>src/App.tsx</code> and save to reload.
+            </Box>
+          </Container>
+        </HeroBody>
+      </Hero>
     );
   }
 }
