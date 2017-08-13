40ce7782b3527c3317736ba9f6716d9846602777
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-08-13 15:20:17 +0300

    Rewrite App.scss in glamorous

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,55 @@
 import * as React from 'react';
-import './App.css';
+
+import glamorous from 'glamorous';
+import * as css from 'glamor';
 
 import { Container, Box, Title, Hero, HeroBody } from 'bloomer';
 
 const logo = require('./logo.svg');
+
+const AppContainer = glamorous(Container)({
+  textAlign: 'center'
+});
+
+const HeaderBox = glamorous(Box)({
+  backgroundColor: '#222',
+  height: '150px',
+  padding: '20px',
+  color: 'white'
+});
+
+const AppLogoSpinAnimation = css.keyframes({
+  from: {
+    transform: 'rotate(0deg)'
+  },
+  to: {
+    transform: 'rotate(360deg)'
+  }
+});
+
+const AppLogo = glamorous.img({
+  animation: `${AppLogoSpinAnimation} infinite 20s linear`,
+  height: '80px'
+});
+
+const IntroBox = glamorous(Box)({
+  fontSize: 'large'
+});
 
 class App extends React.Component {
   render() {
     return (
       <Hero isFullHeight>
         <HeroBody hasTextAlign="centered">
-          <Container className="App">
-            <Box className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
+          <AppContainer>
+            <HeaderBox>
+              <AppLogo alt="logo" src={logo} />
               <Title>Welcome to React</Title>
-            </Box>
-            <Box className="App-intro">
+            </HeaderBox>
+            <IntroBox>
               To get started, edit <code>src/App.tsx</code> and save to reload.
-            </Box>
-          </Container>
+            </IntroBox>
+          </AppContainer>
         </HeroBody>
       </Hero>
     );
