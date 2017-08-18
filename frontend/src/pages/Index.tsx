d2c41aadc4a94d0105717d39fecf3a41d59dac70
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-08-19 01:14:52 +0300

    Render LyricsPreview in Index page Content

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,22 +1,45 @@
 import * as React from 'react';
 
-import { Navbar, NavbarBrand, NavbarItem, Container } from 'bloomer';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarItem,
+  Container,
+  Columns,
+  Column
+} from 'bloomer';
+
+import glamorous from 'glamorous';
+import { serifFamily } from '../theme/typography';
 
 import Layout from '../theme/Layout';
 import Hero from '../theme/Hero';
 
+import LyricsPreview from '../components/LyricsPreview';
+
+const HeroWithNavbar = glamorous(() =>
+  <div>
+    <Hero />
+    <Navbar className="has-shadow">
+      <Container>
+        <NavbarBrand>
+          <NavbarItem className="is-tab is-active">Biisit</NavbarItem>
+          <NavbarItem>Läsyt</NavbarItem>
+        </NavbarBrand>
+      </Container>
+    </Navbar>
+  </div>
+)({
+  fontFamily: serifFamily
+});
+
 export default () =>
   <Layout
-    Navigation={() =>
-      <div>
-        <Hero />
-        <Navbar className="has-shadow">
-          <Container>
-            <NavbarBrand>
-              <NavbarItem className="is-tab is-active">Biisit</NavbarItem>
-              <NavbarItem>Läsyt</NavbarItem>
-            </NavbarBrand>
-          </Container>
-        </Navbar>
-      </div>}
+    Navigation={HeroWithNavbar}
+    Content={() =>
+      <Columns isCentered>
+        <Column isSize={8}>
+          <LyricsPreview />
+        </Column>
+      </Columns>}
   />;
