bd2784e25e45cb3cb5e60e687d7dc342363c326b
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-08-19 01:14:41 +0300

    Add a Content option to Layout

--- frontend/src/theme/Layout.tsx
+++ frontend/src/theme/Layout.tsx
@@ -2,15 +2,29 @@ import * as React from 'react';
 import glamorous from 'glamorous';
 import { sansSerifFamily } from './typography';
 
+import { Tile, Section, Container } from 'bloomer';
+
 export interface Props {
   Navigation: React.ComponentType;
+  Content: React.ComponentType;
 }
 
 const LayoutContainer = glamorous.div({
   fontFamily: sansSerifFamily
 });
 
-export default ({ Navigation }: Props) =>
+export default ({ Navigation, Content }: Props) =>
   <LayoutContainer>
     <Navigation />
+    <Section>
+      <Container>
+        <Tile isAncestor>
+          <Tile isParent>
+            <Tile isChild>
+              <Content />
+            </Tile>
+          </Tile>
+        </Tile>
+      </Container>
+    </Section>
   </LayoutContainer>;
