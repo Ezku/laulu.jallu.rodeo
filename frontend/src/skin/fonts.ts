3f60a616ec5aa75e66a5f86d707caa1d8b7f0863
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-08-19 21:50:25 +0300

    Expose font families as composable glamor css

---
+++ frontend/src/skin/fonts.ts
@@ -0,0 +1,28 @@
+import * as WebFontLoader from 'webfontloader';
+import { css } from 'glamor';
+
+WebFontLoader.load({
+  google: {
+    families: ['Roboto Mono:500']
+  },
+  typekit: {
+    id: 'cae3ngq'
+  }
+});
+
+export const sansSerifFamily =
+  'acumin-pro, BlinkMacSystemFont, -apple-system, "Segoe UI", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif';
+export const serifFamily = 'Adelle, serif';
+export const monospaceFamily = 'Roboto Mono, monospace';
+
+export const sansSerif = css({
+  fontFamily: sansSerifFamily
+});
+
+export const serif = css({
+  fontFamily: serifFamily
+});
+
+export const monospace = css({
+  fontFamily: monospaceFamily
+});
