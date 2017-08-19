3553b7db04b5cd55a816a89b90eb75ea45ad305e
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-08-19 22:26:15 +0300

    Remove dead lines

--- frontend/src/theme/typography.tsx
+++ frontend/src/theme/typography.tsx
@@ -4,11 +4,6 @@ import { serif } from 'app/skin/fonts';
 
 import * as Bloomer from 'bloomer';
 
-// export const sansSerifFamily =
-//   'acumin-pro, BlinkMacSystemFont, -apple-system, "Segoe UI", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif';
-// export const serifFamily = 'Adelle, serif';
-// export const monospaceFamily = 'Roboto Mono, monospace';
-
 export const Title = glamorous(Bloomer.Title)(
   css(serif, {
     fontWeight: 900
