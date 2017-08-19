19e0d713823913a082123e5409044700515a54a2
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-08-19 22:44:33 +0300

    Merge theme/Hero to HeroWithNavbar

--- frontend/src/pages/index/HeroWithNavbar.tsx
+++ frontend/src/pages/index/HeroWithNavbar.tsx
@@ -1,15 +1,33 @@
 import * as React from 'react';
 
 import { Navbar, NavbarBrand, NavbarItem, Container } from 'bloomer';
+import { Column, Input, Control } from 'bloomer';
 
 import glamorous from 'glamorous';
-import Hero from 'app/theme/Hero';
 
 import { serif } from 'app/skin/fonts';
+import { Title, Subtitle } from 'app/theme/typography';
+
+import AwesomeIcon from 'app/components/AwesomeIcon';
+import TwoColumnHero from 'app/components/TwoColumnHero';
+
+const HeroLogoColumn = () =>
+  <Column>
+    <Title>laulu.jallu.rodeo</Title>
+    <Subtitle>Teekkarilaulusanoitusten mekka</Subtitle>
+  </Column>;
+
+const HeroSearchColumn = () =>
+  <Column>
+    <Control hasIcons="right">
+      <Input isSize="large" isColor="primary" placeholder="Etsi biisiä" />
+      <AwesomeIcon isSize="large" isAlign="right" icon="search" />
+    </Control>
+  </Column>;
 
 const HeroWithNavbar = () =>
   <glamorous.Div css={serif}>
-    <Hero />
+    <TwoColumnHero left={<HeroLogoColumn />} right={<HeroSearchColumn />} />
     <Navbar className="has-shadow">
       <Container>
         <NavbarBrand>
