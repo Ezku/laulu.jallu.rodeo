3a95aaadc138d586ba9c65ee7dfa5676f33f4afc
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-08-19 22:14:37 +0300

    Use components/Layout in pages/Index

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,19 +1,37 @@
 import * as React from 'react';
+import { Column, Columns } from 'bloomer';
+import glamorous from 'glamorous';
+import { css } from 'glamor';
 
-import { Columns, Column } from 'bloomer';
-
-import Layout from 'app/theme/Layout';
+import Layout from 'app/components/Layout';
+import { monospace } from 'app/skin/fonts';
 
 import HeroWithNavbar from './index/HeroWithNavbar';
 import LyricsPreview from './index/LyricsPreview';
 
+const CategoryHeader = glamorous.h2(
+  css(monospace, {
+    fontSize: '1.618rem',
+    lineHeight: '2rem',
+    fontWeight: 700,
+    textTransform: 'uppercase',
+    letterSpacing: '0.2rem',
+    marginBottom: '2rem',
+    borderBottom: '1px solid #ddd'
+  })
+);
+
 export default () =>
   <Layout
-    Navigation={HeroWithNavbar}
-    Content={() =>
-      <Columns>
-        <Column isSize={4}>
-          <LyricsPreview />
-        </Column>
-      </Columns>}
+    header={<HeroWithNavbar />}
+    content={
+      <div>
+        <CategoryHeader>Biisit / #parhaat</CategoryHeader>
+        <Columns>
+          <Column isSize={4}>
+            <LyricsPreview />
+          </Column>
+        </Columns>
+      </div>
+    }
   />;
