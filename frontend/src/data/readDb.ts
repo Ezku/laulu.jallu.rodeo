c49b410cd1bb1f5969b2f99349cfd0cc68923cc8
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-09-16 21:48:02 +0300

    Add a readDb helper that performs whitespace cleanup before parsing

---
+++ frontend/src/data/readDb.ts
@@ -0,0 +1,11 @@
+import { songbook } from './parser';
+
+const cleanup = (lines: string) =>
+  lines
+    .split('\n')
+    .map(s => s.trim())
+    .join('\n');
+
+export default function readDb(db: string) {
+  return songbook.db.tryParse(cleanup(db));
+}
