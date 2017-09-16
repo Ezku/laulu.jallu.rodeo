2b50fc6e21db63732d28e09fd7212fc4192703ba
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-09-16 18:10:09 +0300

    Do not use lazy unless actually recursive

--- frontend/src/data/parser.tsx
+++ frontend/src/data/parser.tsx
@@ -1,16 +1,18 @@
 import * as P from 'parsimmon';
 
+const line = P.seqMap(
+  P.digits,
+  P.string('.').then(P.whitespace).then(P.regex(/.+/)),
+  (ordinal, name) => ({
+    ordinal,
+    name
+  })
+);
+
+// tslint:disable-next-line:quotemark
+const lines = P.sepBy(line, P.string("\n"));
+
 export const tableOfContents = {
-  line: P.seqMap(
-    P.digits,
-    P.string('.').then(P.whitespace).then(P.regex(/.+/)),
-    (ordinal, name) => ({
-      ordinal,
-      name
-    })
-  ),
-  lines: P.lazy(() =>
-    // tslint:disable-next-line:quotemark
-    P.sepBy(tableOfContents.line, P.string("\n"))
-  )
+  line,
+  lines
 };
