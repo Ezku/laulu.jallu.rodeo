88863c8b1505b3a88a300000d57c521e145fd7f1
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-10-02 22:50:32 +0300

    Remove asset support attempt from webpack config
    This approach will not work on the server side.
    
    Try https://github.com/zeit/next.js/tree/master/examples/with-global-stylesheet

--- site/next.config.js
+++ site/next.config.js
@@ -1,19 +1,8 @@
 const { createConfig, match, customConfig } = require("@webpack-blocks/webpack");
-const { css, file } = require("@webpack-blocks/assets");
 const typescript = require("@webpack-blocks/typescript");
 
 module.exports = {
   webpack: function(config) {
-    return createConfig([
-      customConfig(config),
-      match("*.css", [css()]),
-      match(["*.js"], [typescript()]),
-      match(
-        ["*.eot", "*.ttf", "*.woff", "*.woff2", "*.svg", "*.css"],
-        [
-          file() // will copy font files to build directory and link to them
-        ]
-      )
-    ]);
+    return createConfig([customConfig(config), match(["*.js"], [typescript()])]);
   }
 };
