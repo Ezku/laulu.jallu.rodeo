ef50e342df8d8cda99bd0cd379389f5346eb8e78
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-10-03 19:08:08 +0300

    Use next/head for including css

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,11 +1,14 @@
 import * as React from "react";
+import Head from "next/head";
 
 import scale from "@laulu.jallu.rodeo/components/dist/utils/scale";
 import { stylesheet } from "./index.css";
 
 export default () => (
   <div>
-    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+    <Head>
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+    </Head>
     hello{" "}
     {Array(12)
       .fill(null)
