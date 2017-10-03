2f00855418971f11b19eed5e6cc0943ac91a72b6
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-10-03 19:07:53 +0300

    Disable useless warning

--- site/postcss.config.js
+++ site/postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   plugins: [
-    require("postcss-cssnext")(),
+    require("postcss-cssnext")({
+      // The warning is from cssnano and should be safe to ignore
+      warnForDuplicates: false
+    }),
     require("postcss-modules")({
       generateScopedName: "[local]-[hash:base64:5]"
     }),
