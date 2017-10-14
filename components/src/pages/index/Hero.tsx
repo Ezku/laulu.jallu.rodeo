4bf68f3ca35c8afd51a73729ba1a405c5cc183a5
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-10-14 23:53:41 +0300

    Switch bloomer Column for glamorous one

--- components/src/pages/index/Hero.tsx
+++ components/src/pages/index/Hero.tsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 import { css } from 'glamor';
 import glamorous from 'glamorous';
 
-import { Column, Input, Control } from 'bloomer';
+import { Input, Control } from 'bloomer';
 import * as Bloomer from 'bloomer';
 
 import hasShadow from '../../skin/hasShadow';
 import { serif } from '../../skin/fonts';
 
+import { Column } from '../../components/Grid';
 import HeroColumns from '../../components/HeroColumns';
 
 const Title = glamorous(Bloomer.Title)(
@@ -25,14 +26,14 @@ const Subtitle = glamorous(Bloomer.Subtitle)(
 );
 
 const HeroLogoColumn = () => (
-  <Column isSize={5}>
+  <Column size={5}>
     <Title>laulu.jallu.rodeo</Title>
     <Subtitle>Salattujen laulujen kirja</Subtitle>
   </Column>
 );
 
 const HeroSearchColumn = () => (
-  <Column isSize={7}>
+  <Column size={7}>
     <Control hasIcons="right" {...css({ display: 'none' })}>
       <Input isSize="large" isColor="primary" placeholder="Etsi sanoitusta" />
       {/* <AwesomeIcon isSize="large" isAlign="right" icon="search" /> */}
