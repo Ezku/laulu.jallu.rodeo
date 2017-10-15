183d66616bd108a948f2b0d68de42d094f0aab0e
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-10-15 11:50:55 +0300

    Simplify Hero

--- components/src/pages/index/Hero.tsx
+++ components/src/pages/index/Hero.tsx
@@ -6,17 +6,13 @@ import { Title, Subtitle } from '../../skin/titles';
 import { Column } from '../../components/Grid';
 import HeroColumns from '../../components/HeroColumns';
 
-const HeroLogoColumn = () => (
-  <Column size={5}>
-    <Title>laulu.jallu.rodeo</Title>
-    <Subtitle>Salattujen laulujen kirja</Subtitle>
-  </Column>
-);
-
 const Hero = () => (
   <div {...hasShadow}>
     <HeroColumns>
-      <HeroLogoColumn />
+      <Column size={12}>
+        <Title>laulu.jallu.rodeo</Title>
+        <Subtitle>Salattujen laulujen kirja</Subtitle>
+      </Column>
     </HeroColumns>
   </div>
 );
