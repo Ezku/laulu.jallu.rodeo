6887d0c8b79cd3b16b6931e458229a44691d0d5d
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-10-15 16:46:21 +0300

    Render site index using Container, Title and Subtitle from components

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -3,14 +3,19 @@ import Head from 'next/head';
 
 import scale from '@laulu.jallu.rodeo/components/dist/utils/scale';
 
+import { Container } from '@laulu.jallu.rodeo/components/dist/components/Grid';
+import { Title, Subtitle } from '@laulu.jallu.rodeo/components/dist/skin/titles';
+
 export default () => (
-  <div>
+  <Container>
     <Head />
-    hello{' '}
-    {Array(12)
-      .fill(null)
-      .map((v, i) => i - 5)
-      .map(scale)
-      .join(', ')}
-  </div>
+    <Title>Hello</Title>
+    <Subtitle>
+      {Array(12)
+        .fill(null)
+        .map((v, i) => i - 5)
+        .map(scale)
+        .join(', ')}
+    </Subtitle>
+  </Container>
 );
