1ac77940e48a96014ed6fa6f986d7af40c9f17aa
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-10-15 17:09:23 +0300

    Set up server side rendering for glamor
    https://github.com/zeit/next.js/tree/master/examples/with-glamorous

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -6,6 +6,16 @@ import scale from '@laulu.jallu.rodeo/components/dist/utils/scale';
 import { Container } from '@laulu.jallu.rodeo/components/dist/components/Grid';
 import { Title, Subtitle } from '@laulu.jallu.rodeo/components/dist/skin/titles';
 
+import { rehydrate, css } from 'glamor';
+import glamorous from 'glamorous';
+
+// Adds server generated styles to glamor cache.
+// Has to run before any `style()` calls
+// '__NEXT_DATA__.ids' is set in '_document.js'
+if (typeof window !== 'undefined') {
+  rehydrate((window as any).__NEXT_DATA__.ids);
+}
+
 export default () => (
   <Container>
     <Head />
