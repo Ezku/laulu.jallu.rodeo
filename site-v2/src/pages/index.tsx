3d48f595bb325f43ddd446f264512be32c6291cf
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-12-03 17:13:51 +0200

    Render laulu.jallu.rodeo index in site-v2

--- site-v2/src/pages/index.tsx
+++ site-v2/src/pages/index.tsx
@@ -1,13 +1,38 @@
-import * as React from 'react'
-import Link from 'gatsby-link'
+import * as React from 'react';
 
-const IndexPage = () => (
+import { Container, Section } from '@laulu.jallu.rodeo/components/dist/components/Layout';
+
+import songbook from '@laulu.jallu.rodeo/data/dist/songbook';
+
+import hasShadow from '@laulu.jallu.rodeo/components/dist/skin/hasShadow';
+import { Title, Subtitle } from '@laulu.jallu.rodeo/components/dist/skin/titles';
+
+import { Column, Columns } from '@laulu.jallu.rodeo/components/dist/components/Grid';
+import HeroColumns from '@laulu.jallu.rodeo/components/dist/components/HeroColumns';
+import SongIndex from '@laulu.jallu.rodeo/components/dist/components/SongIndex';
+import LyricsCardList from '@laulu.jallu.rodeo/components/dist/components/LyricsCardList';
+
+export default () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div {...hasShadow}>
+      <HeroColumns>
+        <Column size={12}>
+          <Title>laulu.jallu.rodeo</Title>
+          <Subtitle>Salattujen laulujen kirja</Subtitle>
+        </Column>
+      </HeroColumns>
+    </div>
+    <Container>
+      <Section>
+        <Columns>
+          <Column size={5}>
+            <SongIndex songs={songbook.tableOfContents} />
+          </Column>
+          <Column size={7}>
+            <LyricsCardList songs={songbook.songs} />
+          </Column>
+        </Columns>
+      </Section>
+    </Container>
   </div>
-)
-
-export default IndexPage
+);
