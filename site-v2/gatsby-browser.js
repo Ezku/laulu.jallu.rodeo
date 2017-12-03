796786efcd88e41bdb3707297a74ab2fd298dcb9
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-12-03 17:40:08 +0200

    Set up server rendering with glamor
    The glamor plugin for gatsby refused to work due to a problem with webpack-configurator so this is copypaste without the gatsby-node.js part

--- site-v2/gatsby-browser.js
+++ site-v2/gatsby-browser.js
@@ -4,4 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
- // You can delete this file if you're not using it
+import { rehydrate } from "glamor"
+
+exports.onClientEntry = () => {
+  if (window._glamor) {
+    rehydrate(window._glamor)
+  }
+}
