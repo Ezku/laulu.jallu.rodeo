91f66f21cfa4501932b93e1cd3aef8befc223920
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-03-04 22:57:21 +0200

    Render plain link to typekit stylesheet in site header

--- site/src/layouts/index.js
+++ site/src/layouts/index.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import 'minireset.css'
-import './index.css'
+import 'minireset.css';
+import './index.css';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet
-      title="laulu.jallu.rodeo"
-    />
+    <Helmet>
+      <title>laulu.jallu.rodeo</title>
+      <link rel="stylesheet" href="https://use.typekit.net/tjr4axo.css" />
+    </Helmet>
     {children()}
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
