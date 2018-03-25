d79305120d006a5aa1aa4ec07e8b1d58172da366
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-03-25 19:30:45 +0300

    Extract Heading component from index and lyrics

---
+++ src/common/components/Heading.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+import hasShadow from '../skin/hasShadow'
+import { Column, Columns } from './Grid'
+import HeroColumns from './HeroColumns'
+import { Title, Subtitle } from '../skin/titles'
+
+export default function Heading() {
+  return (
+    <div {...hasShadow}>
+      <HeroColumns>
+        <Column size={12}>
+          <Title>laulu.jallu.rodeo</Title>
+          <Subtitle>Salattujen laulujen kirja</Subtitle>
+        </Column>
+      </HeroColumns>
+    </div>
+  )
+}
