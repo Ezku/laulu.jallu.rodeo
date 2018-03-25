98e3829e02624be1a11ad964ea1adffbf416664b
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-03-25 19:05:10 +0300

    Document slugify process

--- src/data/utils/slugify.ts
+++ src/data/utils/slugify.ts
@@ -2,10 +2,10 @@ import * as unorm from 'unorm';
 
 export default function slugify(s: string): string {
   return unorm
-    .nfkd(s)
-    .replace(/[^\x00-\x7F]/g, '')
-    .replace(/[^A-Za-z0-9]+/g, '-')
-    .replace(/^([-]*)([^-].*[^-])([-]*)$/g, '$2')
-    .replace(/[-]{2,}/g, '-')
+    .nfkd(s) // Decompose unicode characters (ä becomes a + diaeresis)
+    .replace(/[^\x00-\x7F]/g, '') // Remove non-ascii characters
+    .replace(/[^A-Za-z0-9]+/g, '-') // Replace non-alnum characters with dashes
+    .replace(/^([-]*)([^-].*[^-])([-]*)$/g, '$2') // Trim dashes
+    .replace(/[-]{2,}/g, '-') // Remove repeating dashes
     .toLowerCase();
 }
