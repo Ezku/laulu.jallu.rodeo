1a10c6bffd9d9389249e23243ed43918a23e908d
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-03-25 19:04:35 +0300

    Remove non-unicode characters before dasherizing

--- src/data/utils/slugify.ts
+++ src/data/utils/slugify.ts
@@ -3,6 +3,7 @@ import * as unorm from 'unorm';
 export default function slugify(s: string): string {
   return unorm
     .nfkd(s)
+    .replace(/[^\x00-\x7F]/g, '')
     .replace(/[^A-Za-z0-9]+/g, '-')
     .replace(/^([-]*)([^-].*[^-])([-]*)$/g, '$2')
     .replace(/[-]{2,}/g, '-')
