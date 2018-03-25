80da5508f92b29d42bcacd2b75b594764950affc
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-03-25 18:54:06 +0300

    Normalize string before slugification

--- src/data/utils/slugify.ts
+++ src/data/utils/slugify.ts
@@ -1,5 +1,8 @@
+import * as unorm from 'unorm';
+
 export default function slugify(s: string): string {
-  return s
+  return unorm
+    .nfkd(s)
     .replace(/[^A-Za-z0-9]+/g, '-')
     .replace(/^([-]*)([^-].*[^-])([-]*)$/g, '$2')
     .replace(/[-]{2,}/g, '-')
