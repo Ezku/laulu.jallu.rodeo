ed80446b6c87899717b17da6a386d811d6dc7f2b
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-03-25 20:01:05 +0300

    Fix lint for getSearchParam

--- src/common/utils/getSearchParam.ts
+++ src/common/utils/getSearchParam.ts
@@ -1,4 +1,5 @@
 export default function getSearchParam(searchString: string, paramName: string): string | null {
+  // tslint:disable-next-line
   return typeof URLSearchParams !== 'undefined'
     ? new URLSearchParams(searchString).get(paramName)
     : null
