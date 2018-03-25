e8cbf468b4fb79cea6875ae8db0d4f996c68460d
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-03-25 11:12:43 +0300

    Better slugify

--- src/data/utils/slugify.ts
+++ src/data/utils/slugify.ts
@@ -1,3 +1,7 @@
 export default function slugify(s: string): string {
-  return s.replace(/[^A-Za-z0-9]+/g, '-').toLowerCase();
+  return s
+    .replace(/[^A-Za-z0-9]+/g, '-')
+    .replace(/^([-]*)([^-].*[^-])([-]*)$/g, '$2')
+    .replace(/[-]{2,}/g, '-')
+    .toLowerCase();
 }
