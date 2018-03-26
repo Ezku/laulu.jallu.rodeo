835c7c2dcffa88434e4b46220578aea39ad17236
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-03-26 21:24:44 +0300

    Render Heading in plain Container + Section wrapping

--- src/common/components/Heading.tsx
+++ src/common/components/Heading.tsx
@@ -1,19 +1,15 @@
 import * as React from 'react'
 
-import hasShadow from '../skin/hasShadow'
-import { Column } from './Grid'
-import HeroColumns from './HeroColumns'
 import { Title, Subtitle } from '../skin/titles'
+import { Container, Section } from './Layout'
 
 export default function Heading() {
   return (
-    <div {...hasShadow}>
-      <HeroColumns>
-        <Column size={12}>
-          <Title>laulu.jallu.rodeo</Title>
-          <Subtitle>Salattujen laulujen kirja</Subtitle>
-        </Column>
-      </HeroColumns>
-    </div>
+    <Container>
+      <Section>
+        <Title>laulu.jallu.rodeo</Title>
+        <Subtitle>Salattujen laulujen kirja</Subtitle>
+      </Section>
+    </Container>
   )
 }
