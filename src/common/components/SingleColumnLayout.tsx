aead129bedfd73f0554f4915af728a459f9a5c77
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-03-26 22:33:33 +0300

    Hack over body column size with a min-width

--- src/common/components/SingleColumnLayout.tsx
+++ src/common/components/SingleColumnLayout.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react'
+import { css } from 'glamor'
+
+import { narrow } from '../skin/breakpoints'
 
 import { Container, Section } from './Layout'
 import { Column, Columns } from './Grid'
@@ -7,6 +10,10 @@ export type Props = {
   head: React.ReactNode
   body: React.ReactNode
 }
+
+const minimumColumnWidth = narrow(() => ({
+  minWidth: '30rem'
+}))
 
 export default function SingleColumnLayout(props: Props) {
   return (
@@ -21,7 +28,9 @@ export default function SingleColumnLayout(props: Props) {
       <Container>
         <Section>
           <Columns centered>
-            <Column size={6}>{props.body}</Column>
+            <Column size={6} {...css(minimumColumnWidth)}>
+              {props.body}
+            </Column>
           </Columns>
         </Section>
       </Container>
