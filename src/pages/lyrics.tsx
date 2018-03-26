f1c7380168d2cd310c6f668e391b6db9fd6b3a41
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-03-26 22:02:20 +0300

    Use SingleColumnLayout for lyrics page

--- src/pages/lyrics.tsx
+++ src/pages/lyrics.tsx
@@ -1,35 +1,28 @@
 import * as React from 'react'
 
 import Heading from '../common/components/Heading'
-import { Container, Section } from '../common/components/Layout'
+import SingleColumnLayout from '../common/components/SingleColumnLayout'
 
 import songbook from '../data/songbook'
 import SongBySlug from '../data/SongBySlug'
 import { Song } from '../data/types'
 
-import { Column, Columns } from '../common/components/Grid'
 import LyricsCard from '../common/components/LyricsCard'
 import getSearchParam from '../common/utils/getSearchParam'
 
 export default function Lyrics(props: { location: { search: string } }) {
   const slug = getSearchParam(props.location.search, 'song')
   return (
-    <div>
-      <Heading />
-      <Container>
-        <Section>
-          <Columns>
-            <Column size={7}>
-              <SongBySlug
-                songs={songbook.songs}
-                slug={slug}
-                whenFound={(song: Song) => <LyricsCard song={song} />}
-              />
-            </Column>
-          </Columns>
-        </Section>
-      </Container>
-    </div>
+    <SingleColumnLayout
+      head={<Heading />}
+      body={
+        <SongBySlug
+          songs={songbook.songs}
+          slug={slug}
+          whenFound={(song: Song) => <LyricsCard song={song} />}
+        />
+      }
+    />
   )
 }
 
