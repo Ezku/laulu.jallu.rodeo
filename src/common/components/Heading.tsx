ddfc183b787a09e0809208fe16584126d045d745
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-03-26 21:25:53 +0300

    Add subtle text shadow to highlight Heading copy

--- src/common/components/Heading.tsx
+++ src/common/components/Heading.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import glamorous from 'glamorous'
+import { css } from 'glamor'
 
 import { Title, Subtitle } from '../skin/titles'
 import { Container, Section } from './Layout'
@@ -8,10 +9,14 @@ const CenteredSection = glamorous(Section)({
   textAlign: 'center'
 })
 
+const whiteTextShadow = {
+  textShadow: '1px 1px white'
+}
+
 export default function Heading() {
   return (
     <Container>
-      <CenteredSection>
+      <CenteredSection {...css(whiteTextShadow)}>
         <Title>laulu.jallu.rodeo</Title>
         <Subtitle>Löydä just se teekkaribiisi</Subtitle>
       </CenteredSection>
